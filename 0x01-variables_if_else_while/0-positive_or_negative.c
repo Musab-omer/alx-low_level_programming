4131301ae233b43855f6991093651b5b4a3bc025
musab-omer <mosab.katry@gmail.com>
2023-08-11 16:18:10 +0000

    Print wither the random number generated is postive , zerro or is negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,15 +21,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("is nagative\n");
+		printf("%d is nagative\n", n);
 	}
 	else
 	{
-		printf("is Zero\n");
+		printf("%d is Zero\n", n);
 	}
 	return (0);
 }
