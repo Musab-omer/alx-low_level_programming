e500ae30da8cddefb13228b9631503fef7cd4739
musab-omer <mosab.katry@gmail.com>
2023-08-11 23:30:08 +0000

    program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,13 +17,15 @@ int main(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
-
-			if (i == 9 && j == 9)
+			if ( i != j)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i == 9 && j == 9)
 				continue;
-			putchar(',');
-			putchar(' ');
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
