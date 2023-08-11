a3ae457e17915c0e91630d4849eebf7207ec8bcd
musab-omer <mosab.katry@gmail.com>
2023-08-11 19:33:50 +0000

    program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,28 @@
  * of base 10 starting from 0, followed by a new line
  *
  * Return: Always 0 (success)
+ *
+ * Converter: - giet input as integer type and
+ * convert it to the it's crosponding
+ * ASCII which lead to char that can be
+ * printed using putchar() function
+ *
+ * @digit: is number we need to convert
  */
+char converter(int digit)
+{
+	return ('0' + digit);
+}
+
 int main(void)
 {
 	int i = 0;
+	char ch;
 
 	while (i < 10)
 	{
-		putchar('0' + i);
+		ch = converter(i);
+		putchar(ch);
 		i++;
 	}
 	printf("\n");
